ccbab276cebc952f035a89a2df5ffa1a3c5c8a0d
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-28 16:23:03 -0800

    removed some typescript

--- components/calendar.js
+++ components/calendar.js
@@ -10,7 +10,6 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import { Fragment, useEffect, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { CheckIcon, ExclamationTriangleIcon } from "@heroicons/react/20/solid";
-import { EventSourceInput } from "@fullcalendar/core/index.js";
 
 export default function Calendar() {
   const [events, setEvents] = useState([
